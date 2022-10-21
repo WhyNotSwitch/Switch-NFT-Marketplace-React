b75d4562dabb59ea77ac357708965b82b24d4912
theMikeyx9 <kelechukwuchristwin@gmail.com>
2022-10-21 19:12:19 +0100

    testing gh pages

---
+++ src/components/ProjData.js
@@ -0,0 +1,20 @@
+import switchLg from "./img/Switch/switch.jpg";
+import Nft3 from "./img/Switch/n3.jpg";
+//import Nft8 from "./img/Switch/n8.jpg";
+import Nft4 from "./img/Switch/n4.jpg";
+
+const ProjData = [
+  {
+    title: "Switch Project 3",
+    img: Nft3,
+    price: "0.15ETH",
+    logo: switchLg,
+  },
+  {
+    title: "Switch Project 4",
+    img: Nft4,
+    price: "0.15ETH",
+    logo: switchLg,
+  },
+];
+export default ProjData;
